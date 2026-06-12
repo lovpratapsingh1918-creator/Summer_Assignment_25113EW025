f8601353499d3972eb697be6958cedf72f08d3d3
lovpratapsingh1918-creator <lovpratapsingh1918@gmail.com>
2026-06-12 13:09:26 +0530

    Added DAY10 programs

---
+++ DAY10/program38.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+int main(){
+    int num , i , j , k;
+    printf("enter the height of pattern=");
+    scanf("%d",&num);
+
+    for ( i = num ; i >= 1 ;  i--){
+        for (k = 1 ; k <= num - i ; k++){
+            printf(" ");
+        }
+        for ( j = 1 ; j <= 2*i - 1 ; j++){
+        printf("*");
+        }
+        printf("\n");
+    }
+    return 0 ;
+}
---
+++ DAY10/program39.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+int main(){
+    int num , i , j , k;
+    printf("enter the height of pattern=");
+    scanf("%d",&num);
+
+    for ( i = 1 ; i <= num ;  i++){
+        for (k = 1 ; k <= num - i ; k++){
+            printf(" ");
+        }
+        for ( j = 1 ; j <= i; j++){
+        printf("%d",j);
+        }
+        for ( j = i - 1 ; j >= 1 ; j--){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0 ;
+}
